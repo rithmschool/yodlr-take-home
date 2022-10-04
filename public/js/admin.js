74937da26d49548f7fe49264b5764afab94fe6b0
Christien Ng <ngchristien@gmail.com>
2022-10-03 19:02:39 -0700

    add edit user form modal, move delete button to modal

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,65 +1,87 @@
 const $message = $('#message');
 const $userTable = $('#userTable');
 let userId;
+let values = [];
 
 $(document).ready(async () => {
+  // RENDERS USER INFO ON PAGE LOAD
   const users = await axios.get(`/users`);
   for (let user of users.data)
     $userTable.append(
       addUser(user.id, user.email, user.firstName, user.lastName, user.state)
     );
 
-  $('#userTable tr #delete').on('click', async function () {
-    userId = $(this).closest('tr').data('id');
+  $('#userTable tr').on('click', function () {
+    // GET DATA FROM CLICKED ROW
+    var $row = $(this).closest('tr');
+    var $columns = $row.find('td');
+    values = [];
+    $.each($columns, function (i, item) {
+      values.push(item.innerText);
+    });
+
+    // SHOW MODAL WITH EDIT FORM
+    jQuery.noConflict(); //https://stackoverflow.com/a/34870788
+    $('#myModal').modal('show');
+
+    // SET PRE-FILL USER DATA IN EDIT USER FORM
+    $('#emailInput').val(values[1]);
+    $('#firstNameInput').val(values[2]);
+    $('#lastNameInput').val(values[3]);
+  });
+
+  // PUT /users/:id
+  $('#editForm').on('submit', async evt => {
+    evt.preventDefault();
+    // GET FORM DATA FOR AXIOS REQ
+    let body = {
+      id: values[0],
+      email: $('#emailInput').val(),
+      firstName: $('#firstNameInput').val(),
+      lastName: $('#lastNameInput').val(),
+      state: values[4]
+    };
+
     try {
-      const req = await axios.delete(`/users/${userId}`);
+      const req = await axios.put(`/users/${body.id}`, body);
+      if (req.status === 200) {
+        $message.addClass('alert-success');
+        $message.text('User info saved');
+        $('#myModal').modal('hide');
+        window.location.reload();
+      }
+    } catch (err) {
+      $message.addClass('alert-danger');
+      $message.text('Edit failed, try again.');
+    }
+  });
+
+  // DELETE /users/:id
+  $('#delete').on('click', async () => {
+    try {
+      const req = await axios.delete(`/users/${values[0]}`);
       if (req.status === 204) {
         $message.addClass('alert-success');
         $message.text('User removed');
-        $(this).closest('tr').remove();
+        $('#userTable tr[data-id="' + values[0] + '"]').remove(); //https://stackoverflow.com/a/40374552
+        $('#myModal').modal('hide');
       }
     } catch (err) {
       $message.addClass('alert-danger');
       $message.text('Delete failed, try again.');
     }
   });
-
-  // $userRow.on('click', async evt => {
-  //   console.log(evt.target);
-  // .closest('tr').attr('data-id')
-  // get the current row
-  // var currentRow = $(this).closest('tr');
-
-  // var col1 = currentRow.find($(this).closest('td')).html(); // get current row 1st table cell TD value
-  // var col2 = currentRow.find('.pd-name').html(); // get current row 2nd table cell TD value
-
-  // var data = col1 + '\n' + col2;
-
-  // try {
-  //   const req = await axios.delete(`/users/${evt.id}`);
-  //   if (req.status === 200) {
-  //     $message.addClass('alert-success');
-  //     $message.text('Registration Successful!');
-  //   }
-  // } catch (err) {
-  //   $message.addClass('alert-danger');
-  //   $message.text('Registration failed. Please try again.');
-  // }
-  // });
 });
 
 // accepts props and returns a string of HTML
 addUser = (id, emailInput, firstNameInput, lastNameInput, state) => {
   return `
-      <tr data-id=${id} id="userRow">
-          <td id="id">${id}</td>
+      <tr data-id=${id}>
+          <td>${id}</td>
           <td>${emailInput}</td>
           <td>${firstNameInput}</td>
           <td>${lastNameInput}</td>
           <td>${state}</td>
-          <td id="delete">
-            <button class="btn btn-danger btn-sm">X</button>
-          </td>
       </tr>
       `;
 };
